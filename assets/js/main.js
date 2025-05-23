3e49efd53e222030ef0d2665ab1b06c7ba99333e
theShinigami <theShinigami@users.noreply.github.com>
2025-05-23 09:01:02 +0000

    deploy: 82121e504aa278da2e9241a7f5a5ea33e252fb5d

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+/*
+  Mobile site navigation
+*/
+const siteNav = document.querySelector('.site-nav');
+
+if (siteNav) {
+  const navToggler = document.getElementById('NavToggler');
+  const allMenus = siteNav.querySelectorAll('[data-menu-list]');
+
+  navToggler.addEventListener('click', () => {
+    const siteMain = document.getElementById('SiteMain');
+    const siteFooter = document.getElementById('SiteFooter');
+
+    if (siteNav.classList.contains('hidden')) {
+      siteNav.classList.replace('hidden', 'flex');
+    } else {
+      siteNav.classList.replace('flex', 'hidden');
+    }
+
+    /*
+      Hide the main content and footer when the mobile menu is open.
+      This allows the menu to be scrollable and limits the DOM for screen readrs.
+    */
+    siteMain.classList.toggle('hidden');
+    siteFooter.classList.toggle('hidden');
+  });
+
+  const navMenuTriggers = document.querySelectorAll('[data-menu-trigger]');
+
+  navMenuTriggers.forEach(trigger => {
+    trigger.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      const target = trigger.nextElementSibling;
+
+      navMenuTriggers.forEach((trigger) => {
+        trigger.setAttribute('aria-expanded', 'false');
+      });
+
+      allMenus.forEach((menu) => {
+        if (menu !== target) {
+          menu.classList.replace('flex', 'hidden');
+        }
+      });
+
+      if (target.classList.contains('hidden')) {
+        target.classList.replace('hidden', 'flex');
+        trigger.setAttribute('aria-expanded', 'true');
+      } else {
+        target.classList.replace('flex', 'hidden');
+        trigger.setAttribute('aria-expanded', 'false');
+      }
+    });
+  });
+
+  // Close all menus when the user clicks outside
+  document.addEventListener('click', function (evt) {
+    if (!siteNav.contains(evt.target) && navToggler !== evt.target) {
+      // Close all menus if you click outside of menu
+      allMenus.forEach((menu) => {
+        menu.classList.replace('flex', 'hidden');
+      });
+
+      navMenuTriggers.forEach((trigger) => {
+        trigger.setAttribute('aria-expanded', 'false');
+      });
+    }
+  });
+}
